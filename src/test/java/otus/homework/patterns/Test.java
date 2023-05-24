26be20c2329954062d55166bcdb58598c3f3e886
Italia1235 <1437dada@mail.ru>
2023-05-24 22:37:52 +0600

    add tests

---
+++ src/test/java/otus/homework/patterns/Test.java
@@ -0,0 +1,12 @@
+package otus.homework.patterns;
+
+import org.springframework.util.Assert;
+
+public class Test {
+
+    boolean m = true;
+    @org.junit.jupiter.api.Test
+    public void test(){
+       Assert.isTrue(m,"это верно");
+    }
+}
